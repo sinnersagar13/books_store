7fcbf33592d2279f9990e7a3932df53ed4f5f5b9
sinnersagar13 <72201934+sinnersagar13@users.noreply.github.com>
2022-10-14 09:07:18 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Bookstore',
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       theme: ThemeData.dark().copyWith(primaryColor: Colors.blue),
       home: HomePageScreen(),
       // routes: {
